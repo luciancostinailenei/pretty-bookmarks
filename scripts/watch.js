344bacfca101ede0f4a2465d35fce688cf53707d
luciancostin.ailenei <luciancostin.ailenei@gmail.com>
2023-02-20 19:09:06 +0200

    refactor: eject webpack config; add watch script for live reload on Chrome extension

---
+++ scripts/watch.js
@@ -0,0 +1,32 @@
+process.env.BABEL_ENV = "development";
+process.env.NODE_ENV = "development";
+
+const fs = require("fs-extra");
+const paths = require("../config/paths");
+const webpack = require("webpack");
+const configFactory = require("../config/webpack.config");
+const config = configFactory("development");
+
+config.output.path = paths.appBuild;
+paths.publicUrl = paths.appBuild + "/";
+
+webpack(config).watch({}, (err, stats) => {
+  if (err) {
+    console.error(err);
+  } else {
+    copyPublicFolder();
+  }
+  console.error(
+    stats.toString({
+      chunks: false,
+      colors: true,
+    })
+  );
+});
+
+function copyPublicFolder() {
+  fs.copySync(paths.appPublic, paths.appBuild, {
+    dereference: true,
+    filter: (file) => file !== paths.appHtml,
+  });
+}
