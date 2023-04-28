a2085ab48aa3471fd5ff3fdc563d12ccea87f9d1
luciancostin.ailenei <luciancostin.ailenei@gmail.com>
2023-04-28 23:52:12 +0300

    style: go to top icon styling adjustments

--- src/components/ScrollToTop/ScrollToTop.tsx
+++ src/components/ScrollToTop/ScrollToTop.tsx
@@ -6,42 +6,42 @@ import styles from "../../layouts/ScrollToTop/ScrollToTop.module.css";
 import { ChevronUpIcon } from "@chakra-ui/icons";
 
 const ScrollToTop = () => {
-    const { colorMode } = useColorMode();
+  const { colorMode } = useColorMode();
 
-    const [showTopBtn, setShowTopBtn] = useState(false);
-    useEffect(() => {
-        window.addEventListener("scroll", () => {
-            if (window.scrollY > 100) {
-                setShowTopBtn(true);
-            } else {
-                setShowTopBtn(false);
-            }
-        });
-    }, []);
+  const [showTopBtn, setShowTopBtn] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        setShowTopBtn(true);
+      } else {
+        setShowTopBtn(false);
+      }
+    });
+  }, []);
 
-    const goToTop = () => {
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth",
-        });
-    };
+  const goToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
 
-    return (
-        <div className="top-to-btn">
-            {showTopBtn && (
-                <Icon as={ChevronUpIcon} 
-                    w={50} 
-                    h={50}
-                    className={cn(
-                        styles["icon"],
-                        colorMode === "dark"
-                          ? styles["icon--dark"]
-                          : styles["icon--light"],
-                      )}
-                    onClick={goToTop}
-                />
-            )}
-        </div>
-    );
+  return (
+    <div className="top-to-btn">
+      {showTopBtn && (
+        <Icon
+          color="white"
+          as={ChevronUpIcon}
+          w={25}
+          h={25}
+          className={cn(
+            styles["icon"],
+            colorMode === "dark" ? styles["icon--dark"] : styles["icon--light"]
+          )}
+          onClick={goToTop}
+        />
+      )}
+    </div>
+  );
 };
 export default ScrollToTop;
